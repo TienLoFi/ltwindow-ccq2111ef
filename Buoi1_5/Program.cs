0870a6f479fe30de52958bbcec368aae3cbf2a8f
TienLoFi <ngoctiendz2k3@gmail.com>
2023-09-24 14:45:25 +0700

    laptrinhwindowef

---
+++ Buoi1_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+          Console.Write("Nhap so luong sinh vien: ");
+            int n = int.Parse(Console.ReadLine());
+
+        string[] danhSachSinhVien = NhapSV(n);
+
+        Console.WriteLine("\nDanh sach sinh vien:");
+            XuatSV(danhSachSinhVien, n);
+
+            Console.ReadKey();
+        }
+     
+    static string[] NhapSV(int n)
+    {
+        string[] danhSachSinhVien = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap ho ten sinh vien {0}: ", i + 1);
+            danhSachSinhVien[i] = Console.ReadLine();
+        }
+
+        return danhSachSinhVien;
+    }
+
+    static void XuatSV(string[] danhSachSinhVien, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Sinh vien {0}: {1}", i + 1, danhSachSinhVien[i]);
+        }
+
+    }
+
+}
+}
